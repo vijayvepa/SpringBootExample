034cf7ac24ee70fe286e24edc57f4819b3fb7756
Vijay <vijay.k.vepakomma@gmail.com>
2022-10-19 13:12:21 -0400

    add aircraft demo

---
+++ aircraft-demo/src/main/java/com/vijayepa/aircraft/AircraftDemoApplication.java
@@ -0,0 +1,13 @@
+package com.vijayepa.aircraft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AircraftDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AircraftDemoApplication.class, args);
+	}
+
+}
