0d84ad2114dc623adf3e7301c2027d3e23cd950a
Vijay <vijay.k.vepakomma@gmail.com>
2022-10-23 08:54:01 -0400

    spring init spring-reactive-web spring-data-neo4j

---
+++ plane-reporter-neo4j/src/main/java/com/vijayvepa/planereporterneo4j/PlaneReporterNeo4jApplication.java
@@ -0,0 +1,13 @@
+package com.vijayvepa.planereporterneo4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaneReporterNeo4jApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaneReporterNeo4jApplication.class, args);
+	}
+
+}
