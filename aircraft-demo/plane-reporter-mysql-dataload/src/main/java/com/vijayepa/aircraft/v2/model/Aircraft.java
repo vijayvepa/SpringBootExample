b31209c9838a5caeea90b317f58466fad40c093a
Vijay <vijay.k.vepakomma@gmail.com>
2022-10-23 07:32:42 -0400

    auto schema setup example

---
+++ aircraft-demo/plane-reporter-mysql-dataload/src/main/java/com/vijayepa/aircraft/v2/model/Aircraft.java
@@ -0,0 +1,70 @@
+package com.vijayepa.aircraft.v2.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.Hibernate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.Instant;
+import java.util.Objects;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Entity
+public class Aircraft {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String callsign, squawk, reg, flightno, route, type, category;
+
+    private int altitude, heading, speed;
+    @JsonProperty("vert_rate")
+    private int vertRate;
+    @JsonProperty("selected_altitude")
+    private int selectedAltitude;
+
+    private double lat, lon, barometer;
+    @JsonProperty("polar_distance")
+    private double polarDistance;
+    @JsonProperty("polar_bearing")
+    private double polarBearing;
+
+    @JsonProperty("is_adsb")
+    private boolean isADSB;
+    @JsonProperty("is_on_ground")
+    private boolean isOnGround;
+
+    @JsonProperty("last_seen_time")
+    private Instant lastSeenTime;
+    @JsonProperty("pos_update_time")
+    private Instant posUpdateTime;
+    @JsonProperty("bds40_seen_time")
+    private Instant bds40SeenTime;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Aircraft aircraft = (Aircraft) o;
+        return id != null && Objects.equals(id, aircraft.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
