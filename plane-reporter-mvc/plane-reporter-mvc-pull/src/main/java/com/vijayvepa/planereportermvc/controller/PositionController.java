d77d4b91f2f493edcba36b84fc06879e3ce4a763
Vijay <vijay.k.vepakomma@gmail.com>
2022-10-24 07:17:52 -0400

    use PositionService

--- plane-reporter-mvc/plane-reporter-mvc-pull/src/main/java/com/vijayvepa/planereportermvc/controller/PositionController.java
+++ plane-reporter-mvc/plane-reporter-mvc-pull/src/main/java/com/vijayvepa/planereportermvc/controller/PositionController.java
@@ -1,30 +1,24 @@
 package com.vijayvepa.planereportermvc.controller;
 
-import com.vijayvepa.planereportermvc.dataaccess.AircraftRepository;
 import com.vijayvepa.planereportermvc.model.Aircraft;
+import com.vijayvepa.planereportermvc.service.PositionService;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.reactive.function.client.WebClient;
 
 @RequiredArgsConstructor
 @Controller
 public class PositionController {
 
     @NonNull
-    private final AircraftRepository aircraftRepository;
-    private WebClient client = WebClient.create("http://localhost:7634/aircraft");
+    private final PositionService positionService;
 
     @GetMapping("/aircraft")
     public String getCurrentAircraftPositions(Model model){
-        aircraftRepository.deleteAll();
-
-        client.get().retrieve().bodyToFlux(Aircraft.class).filter(plane -> !plane.getReg().isEmpty())
-                .toStream()
-                .forEach(aircraftRepository::save);
-        model.addAttribute("currentPositions", aircraftRepository.findAll());
+        final Iterable<Aircraft> currentAircraftPositions = positionService.getCurrentAircraftPositions();
+        model.addAttribute("currentPositions", currentAircraftPositions);
 
         return "positions"; //.html
     }
