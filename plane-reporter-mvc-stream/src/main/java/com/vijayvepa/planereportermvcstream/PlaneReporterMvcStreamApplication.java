7ae80e8c29103388606a60d6fe5018b65633a1e4
Vijay <vijay.k.vepakomma@gmail.com>
2022-10-24 07:35:32 -0400

    added spring-cloud-stream

---
+++ plane-reporter-mvc-stream/src/main/java/com/vijayvepa/planereportermvcstream/PlaneReporterMvcStreamApplication.java
@@ -0,0 +1,13 @@
+package com.vijayvepa.planereportermvcstream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaneReporterMvcStreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaneReporterMvcStreamApplication.class, args);
+	}
+
+}
---
+++ plane-reporter-mvc-stream/src/test/java/com/vijayvepa/planereportermvcstream/PlaneReporterMvcStreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vijayvepa.planereportermvcstream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaneReporterMvcStreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
