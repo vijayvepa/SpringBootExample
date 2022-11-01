6aba33bb07aa879515504f45abc4bc915667fa25
Vijay <vijay.k.vepakomma@gmail.com>
2022-11-01 12:41:20 -0400

    plane-finder-oauth

---
+++ aircraft-demo/plane-finder-oauth/src/main/java/com/vijayvepa/plane/PlaneFinderApplication.java
@@ -0,0 +1,13 @@
+package com.vijayvepa.plane;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaneFinderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlaneFinderApplication.class, args);
+    }
+
+}
---
+++ plane-reporter-jpa-oauth/src/main/java/com/vijayvepa/planereporterjpaoauth/PlaneReporterJpaOauthApplication.java
@@ -0,0 +1,13 @@
+package com.vijayvepa.planereporterjpaoauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaneReporterJpaOauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaneReporterJpaOauthApplication.class, args);
+	}
+
+}
