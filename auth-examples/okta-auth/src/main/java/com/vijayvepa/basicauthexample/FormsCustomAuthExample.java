55e6f9e7e2ece765fa6d4981ce8d645febf1cf83
Vijay <vijay.k.vepakomma@gmail.com>
2022-11-03 12:28:12 -0400

    okta auth - copy of forms auth custom

---
+++ auth-examples/okta-auth/src/main/java/com/vijayvepa/basicauthexample/FormsCustomAuthExample.java
@@ -0,0 +1,13 @@
+package com.vijayvepa.basicauthexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormsCustomAuthExample {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormsCustomAuthExample.class, args);
+	}
+
+}
